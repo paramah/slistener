9aa87a9a6fea7e65cd586795bcf1e8f66c16881b
Aleksander Cynarski <aleksander@cynarski.pl>
2019-06-15 14:06:07 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# Supervisord simple event listener
+
+### Configuration
+
+Environment variables:
+
+- ** ***********EVENT** (required) supervisord event name
+- **PROCESSNAME** (required) process name
+- **DELAY** (required) delay in seconds
+- **EXECUTE** (required) command to execute
+
+supervisord configuration example:
+
+```
+[program:php-fpm]
+command=/usr/sbin/php-fpm7.2 -R -F
+autorestart=true
+stdout_logfile=/dev/stdout
+stdout_logfile_maxbytes=0
+redirect_stderr=true
+
+
+[eventlistener:php-fpm-listener]
+command=<path_to>/listener.py
+numprocs=1
+events=PROCESS_STATE_STARTING
+autorestart=true
+stderr_logfile=errorlogfile
+stdout_logfile=applogfile
+environment=DEBUG="true",EVENT="PROCESS_STATE_STARTING",PROCESNAME="php-fpm",DELAY="2","EXECUTE"="/bin/echo testing"
+```
---
+++ listener.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python
+
+import sys
+import os
+import logging
+import subprocess
+import time
+
+
+from supervisor.childutils import listener
+
+
+def main(args):
+    logging.basicConfig(stream=sys.stderr, level=logging.DEBUG, format='%(asctime)s %(levelname)s %(filename)s: %(message)s')
+    logger = logging.getLogger("supervisord-eventlistener")
+    debug_mode = True if 'DEBUG' in os.environ else False
+    
+    if 'PROCESNAME' in os.environ:
+        processName = os.environ["PROCESNAME"]
+    else: 
+        logger.critical("Set PROCESNAME in environment!");
+        exit(1)
+
+    if 'EVENT' in os.environ:
+        eventName = os.environ["EVENT"]
+    else:
+        logger.critical("Set EVENT in environment!")
+        exit(1)
+
+    if 'EXECUTE' in os.environ:
+        executeCommand = os.environ["EXECUTE"].split(" ")
+    else:
+        logger.critical("Set EXECUTE in environment!")
+        exit(1)
+
+    if 'DELAY' in os.environ:
+        sleepTime = int(os.environ["DELAY"])
+    else:
+        logger.critical("Set DELAY in environment!")
+        exit(1)
+
+    while True:
+        headers, body = listener.wait(sys.stdin, sys.stdout)
+        body = dict([pair.split(":") for pair in body.split(" ")])
+
+        if debug_mode: 
+            logger.debug("ENV: %r", repr(os.environ))
+            logger.debug("Headers: %r", repr(headers))
+            logger.debug("Body: %r", repr(body))
+            logger.debug("Args: %r", repr(args))
+
+        try:
+            if headers["eventname"] == eventName and body["processname"] == processName:
+                if debug_mode:
+                    logger.debug("Process %s entered RUNNING state...", processName)
+                time.sleep(sleepTime); 
+                if debug_mode:
+                    logger.debug("Execute %s after %s (sec)...", os.environ["EXECUTE"], os.environ["DELAY"])
+                res = subprocess.call(executeCommand, stdout=sys.stderr)
+        except Exception as e:
+            logger.critical("Unexpected Exception: %s", str(e))
+            listener.fail(sys.stdout)
+            exit(1)
+        else:
+            listener.ok(sys.stdout)
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
